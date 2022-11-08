c0dd5a0641c96c8460347c68652a4f94459ac21b
Alex250303 <71701264+Alex250303@users.noreply.github.com>
2022-11-08 17:32:58 +0200

    upl lab7

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Task1 from './components/Task1/Task1';
+import Task2 from './components/Task2/Task2';
+
+function App() {
+  return (
+    <div className="App">
+      <Task1 />
+      <Task2 />
+    </div>
+  );
+}
+
+export default App;
